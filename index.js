5ae64764536c76c3e1281df1b0cc38de8491194d
Aria Stewart <aredridel@nbtsc.org>
2015-06-14 14:59:26 -0400

    Add debug logging

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var bcp47 = require('bcp47');
+var debug = require('debuglog')('express-bcp47');
 
 module.exports = function (options) {
     options = options || {};
@@ -8,11 +9,15 @@ module.exports = function (options) {
     }
 
     return function handlebcp47(req, res, next) {
-        if (!(res.locals.locale = req.locale = bcp47.parse(req.headers['accept-language']))) {
+        if ((res.locals.locale = req.locale = bcp47.parse(req.headers['accept-language']))) {
+            debug("locale selected from accept-language header is '%s'", req.headers['accept-language']);
+        } else {
             res.locals.locale = req.locale = bcp47.parse(options.defaultLocale);
+            debug("using default locale '%s'", options.defaultLocale);
         }
 
         if (options.vary) {
+            debug("signalling that we vary our output based on the accept-language header for caching purposes");
             res.setHeader('vary', 'accept-language');
         }
 
